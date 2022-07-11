cd95d68ef42d172887eb3d49154d58e7626ca87b
Joshua Byrd <phocks@gmail.com>
2022-07-11 18:05:26 +1000

    Some kinda top nav

---
+++ src/components/TopNav.svelte
@@ -0,0 +1,29 @@
+<div class="top-nav">
+  <span class="item brand">
+    <strong><a href="/">home</a></strong> /
+  </span>
+</div>
+
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=PT+Serif&display=swap");
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  .top-nav {
+    min-height: 5rem;
+    display: flex;
+    align-items: center;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    font-size: 1.333rem;
+    font-family: "PT Serif", serif;
+    background-color: indianred;
+  }
+
+  .item {
+    margin-right: 1rem;
+  }
+</style>
