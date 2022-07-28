788e06e7e610c1d8146971fa886f427d4398c59f
Joshua Byrd <phocks@gmail.com>
2022-07-29 07:27:43 +1000

    dash span

---
+++ src/lib/sorting.ts
@@ -0,0 +1,37 @@
+// Types
+import { MarkdownInstance } from "astro";
+import type { QuoteFrontmatter } from "$lib/types";
+
+import * as R from "ramda";
+
+/**
+ * A comparitor function to pass to Ramda. TODO: actually turn to dates.
+ * @param {QuoteFrontmatter} a - The first quote to compare
+ * @param {QuoteFrontmatter} b - The second quote to compare
+ * @returns {number} - The result of the comparison (1 or -1)
+ */
+const diff = (
+  a: MarkdownInstance<QuoteFrontmatter>,
+  b: MarkdownInstance<QuoteFrontmatter>
+) => {
+  // TODO: Deal with missing dateAdded
+  const first = a.frontmatter.dateAdded;
+  const second = b.frontmatter.dateAdded;
+  console.log(first, second);
+  const comparison = second.localeCompare(first);
+  console.log(comparison);
+  return second.localeCompare(first);
+};
+
+/**
+ * Sort quotes by dateAdded
+ * @param quotes - Markdown pages to sort
+ * @returns - The sorted quotes
+ */
+export const sortQuotesDescendingByTime = (
+  quotes: MarkdownInstance<QuoteFrontmatter>[]
+) => {
+  const sortByMostRecent = R.sort(diff);
+  const sortedQuotes = sortByMostRecent(quotes);
+  return sortedQuotes;
+};
