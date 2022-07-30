9a6662a4764a38bb4a7e23c5bc71a60febcd738a
Joshua Byrd <phocks@gmail.com>
2022-07-30 17:55:01 +1000

    fonts and stuff

---
+++ src/lib/quotes/emily-bronte/a-sorrowful-sight-i-saw.md
@@ -0,0 +1,10 @@
+---
+title: A sorrowful sight I saw
+dateAdded: 2019-07-10
+text: "A sorrowful sight I saw: dark night coming down prematurely, and sky and hills mingled in one bitter whirl of wind and suffocating snow."
+author: Emily Brontë
+source: Wuthering Heights
+topics:
+  - Weather
+  - Love
+---
--- src/lib/sorting.ts
+++ src/lib/sorting.ts
@@ -5,23 +5,6 @@ import type { QuoteFrontmatter } from "$lib/types";
 import * as R from "ramda";
 
 /**
- * A comparitor function to pass to Ramda. TODO: actually turn to dates.
- * @param {QuoteFrontmatter} a - The first quote to compare
- * @param {QuoteFrontmatter} b - The second quote to compare
- * @returns {number} - The result of the comparison (1 or -1)
- */
-const diff = (
-  a: MarkdownInstance<QuoteFrontmatter>,
-  b: MarkdownInstance<QuoteFrontmatter>
-) => {
-  // TODO: Deal with missing dateAdded
-  const first = a.frontmatter.dateAdded;
-  const second = b.frontmatter.dateAdded;
-  const comparison = second.localeCompare(first);
-  return comparison;
-};
-
-/**
  * Sort quotes by dateAdded
  * @param quotes - Markdown pages to sort
  * @returns - The sorted quotes
@@ -29,6 +12,23 @@ const diff = (
 export const sortQuotesDescendingByTime = (
   quotes: MarkdownInstance<QuoteFrontmatter>[]
 ) => {
+  /**
+   * A comparitor function to pass to Ramda. TODO: actually turn to dates.
+   * @param {QuoteFrontmatter} a - The first quote to compare
+   * @param {QuoteFrontmatter} b - The second quote to compare
+   * @returns {number} - The result of the comparison (1 or -1)
+   */
+  const diff = (
+    a: MarkdownInstance<QuoteFrontmatter>,
+    b: MarkdownInstance<QuoteFrontmatter>
+  ) => {
+    // TODO: Deal with missing dateAdded values
+    const first = a.frontmatter.dateAdded;
+    const second = b.frontmatter.dateAdded;
+    const comparison = second.localeCompare(first);
+    return comparison;
+  };
+
   const sortByMostRecent = R.sort(diff);
   const sortedQuotes = sortByMostRecent(quotes);
   return sortedQuotes;
