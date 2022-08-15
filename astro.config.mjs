9841fdba5b606586200eef1f10dccd892e02b770
Joshua Byrd <phocks@gmail.com>
2022-08-15 21:19:04 +1000

    tried out Solid but naa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: "https://quoke.top"
+  integrations: [],
+  site: "https://quoke.co"
 });
