625e8c3d178634c3d896fcf0bf2528c9b6c1a457
Joshua Byrd <phocks@gmail.com>
2022-08-16 06:34:46 +1000

    Fix 1.0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+  trailingSlash: 'always',
   site: "https://quoke.co"
 });
