46b0c0405d654266d48a0a3e402e0a6f594f7ec5
Joshua Byrd <phocks@gmail.com>
2022-08-20 14:24:40 +1000

    Footer test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [svelte()],
   trailingSlash: 'always',
   site: "https://quoke.co"
 });
